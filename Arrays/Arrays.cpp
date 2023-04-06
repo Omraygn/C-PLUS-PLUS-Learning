a106acb2216b82d6f81d854902d9fdfeb53580b1
Omraygn <omraygn06@gmail.com>
2023-04-06 20:48:02 +0300

    update

---
+++ Arrays/Arrays.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	 
+	// diziler
+	              //0  1  2  3  4 
+	/*int puanlar[5]{10,20,55,75,100};
+
+	cout << puanlar[3]; 
+	                           */ 
+	
+	
+	
+	
+	// diziler de for kullanýmý
+	
+	/*for (int i = 0; i < 5; i++) {
+
+		cout << "Puanlar" << puanlar [i]  << endl; 
+	}
+	          */ 
+
+
+	//Ýç içe diziler 
+
+	/*int dizi[1][1][1]
+	{
+		{
+
+			{
+				1
+			} 
+
+		}
+
+	};
+
+	cout << dizi[0][0][0];
+
+}                    */ 
+
+
+	 
+              
